f4d18ab0a8bb9bdfe613afd3087a81b555c80bb4
zhanghaoyan05 <1481531316@qq.com>
2026-04-15 22:07:41 +0800

    merge my extraction and KB changes onto latest teammate code

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 from api.rag_api import router as rag_router
 from api.course_api import router as course_router
+from api.syllabus_api import router as syllabus_router
+from api.document_api import router as document_router
+from api.kb_api import router as kb_router
 
 app = FastAPI()
 
 app.include_router(rag_router)
 app.include_router(course_router)
+app.include_router(syllabus_router)
+app.include_router(document_router)
+app.include_router(kb_router)
 
 @app.get("/")
 def root():
